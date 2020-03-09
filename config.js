b5253aef45e761dbedc1e6b3e308d19a9a1857cc
ernisx89 <50714548+ernisx89@users.noreply.github.com>
2020-03-09 13:37:46 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321', //prisijungiant json webtoken
     'mongoUrl': 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '2601859243198667',
-        clientSecret: '61baa6d8d9565b623bace3434081eab6'
+        clientId: 'xxx',
+        clientSecret: 'xxx'
     }
 };
